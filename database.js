8e33b48858e73dc49d310342defffdc05e90d2ab
DenisDS <denis.dsilveira1@outlook.com>
2025-02-25 21:42:25 -0300

    mudança do nome do repositorio/teste

---
+++ database.js
@@ -0,0 +1,11 @@
+/**
+ * Modulo de conexão com banco de dados
+ * Uso do framework mongoose
+ */
+
+//importação do mongoose
+const mongoose = require('mongose')
+
+//Configuração do banco de dados
+
+const url = 'mongodb+srv://denisagotec:<db_password>@cluster0.qfhlm.mongodb.net/'
